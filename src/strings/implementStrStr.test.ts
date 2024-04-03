216eee47737d4fca66cecd013270015f6b607493
Jack Barry <Jack@Barry.onl>
2024-04-03 15:42:44 -0600

    wip: Revise answer for implement strStr

--- src/strings/implementStrStr.test.ts
+++ src/strings/implementStrStr.test.ts
@@ -2,27 +2,7 @@ import { describe, expect, test } from 'bun:test'
 
 // https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/885/
 function strStr(haystack: string, needle: string): number {
-  if (needle.length > haystack.length) {
-    return -1
-  }
-
-  for (let i = 0; i < haystack.length; i++) {
-    if (haystack[i] !== needle[0] || haystack.length - i < needle.length) {
-      continue
-    }
-
-    for (let j = 0; j < needle.length; j++) {
-      if (haystack[i + j] !== needle[j]) {
-        break
-      }
-
-      if (j === needle.length - 1) {
-        return i
-      }
-    }
-  }
-
-  return -1
+  return haystack.indexOf(needle)
 }
 
 describe("strings: implementStrStr", () => {
