4145b8ecca50f4b7301dfa2be94dd0d60d103abc
Jack Barry <Jack@Barry.onl>
2024-04-09 17:01:14 -0600

    wip: Better answer for rotate array

--- src/arrays/rotateArray.test.ts
+++ src/arrays/rotateArray.test.ts
@@ -2,14 +2,34 @@ import { describe, expect, test } from 'bun:test'
 
 // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
 function rotate(nums: number[], k: number): void {
-  for (let i = 0; i < k; i++) {
-    const last = nums.pop()
-    nums.unshift(last as number)
-  }
+  // Make sure k is within bounds of nums 
+  k %= nums.length
+  // Reverse the entire array
+  reverse(nums, 0, nums.length - 1)
+  // Reverse array from beginning up to value just before kth value
+  reverse(nums, 0, k - 1)
+  // Reverse array from kth value to end
+  reverse(nums, k, nums.length - 1)
 };
+
+// O(n) time reversal of given section of array
+function reverse(nums: number[], start: number, end: number) {
+  while (start < end) {
+    let temp = nums[start]
+    nums[start] = nums[end]
+    nums[end] = temp
+
+    start++
+    end--
+  }
+}
 
 describe("arrays: rotateArray", () => {
   test("handles array with odd length and odd k less than length", () => {
+    // Example steps
+    // [7, 6, 5, 4, 3, 2, 1]
+    // [5, 6, 7, 4, 3, 2, 1]
+    // [5, 6, 7, 1, 2, 3, 4]
     const nums = [1, 2, 3, 4, 5, 6, 7]
     rotate(nums, 3)
     expect(nums).toStrictEqual([5, 6, 7, 1, 2, 3, 4])
