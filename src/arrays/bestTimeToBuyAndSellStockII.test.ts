de35cfdc9e310d9eed641fa36df7922f70063b8b
Jack Barry <Jack@Barry.onl>
2024-04-09 11:12:10 -0600

    wip: Re-solve best time to buy and sell stock Uses math trickery instead of greedy algo

--- src/arrays/bestTimeToBuyAndSellStockII.test.ts
+++ src/arrays/bestTimeToBuyAndSellStockII.test.ts
@@ -2,22 +2,13 @@ import { describe, expect, test } from 'bun:test'
 
 function maxProfit(prices: number[]): number {
   let profit = 0
-  let buyPrice = prices[0]
-  let sellPrice = prices[0]
 
-  for (const price of prices) {
-    if (price < sellPrice) {
-      profit += sellPrice - buyPrice
-      buyPrice = price
-      sellPrice = price
-    }
-
-    if (price > buyPrice) {
-      sellPrice = price
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < prices[i + 1]) {
+      profit += prices[i + 1] - prices[i]
     }
   }
 
-  profit += Math.max(sellPrice - buyPrice, 0)
   return profit
 };
 
